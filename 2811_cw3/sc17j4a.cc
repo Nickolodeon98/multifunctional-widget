7b8bd39e699d519d1a5cfcd11520e8c7545fcee8
Jack Auty <sc17j4a@leeds.ac.uk>
2018-11-30 21:06:37 +0000

    Started layout of tab, added search box

--- 2811_cw3/sc17j4a.cc
+++ 2811_cw3/sc17j4a.cc
@@ -1,13 +1,31 @@
 #include "globals.h"
 
 #include <QLabel>
+#include <string>
+#include <iostream>
+#include "gitpp.h"
+#include <QGridLayout>
+#include <QtWidgets>
+#include <QLineEdit>
 
-namespace{
+namespace searchCommits{
 
 class HelloWorldLabel : public QLabel{
 public:
 	HelloWorldLabel() : QLabel(){
-		setText("Jacks Tab");
+		// setText("Jacks Tab");
+		GITPP::REPO r;
+
+		QLineEdit* textField=new QLineEdit();
+		textField->setFont(QFont("Typewriter", 25));
+		textField->setPlaceholderText("Enter your search terms here");
+
+		QGridLayout* layout = new QGridLayout();
+		layout->addWidget(textField, 0, 0);
+		// layout->addWidget(searchResults, 1, 0);
+
+
+		setLayout(layout);
 	}
 };
 
