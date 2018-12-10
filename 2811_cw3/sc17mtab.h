dae4e51aeb1b401e4feb18aec4069839c88d2082
Matthew Barrs <sc17mtab@leeds.ac.uk>
2018-12-10 15:57:37 +0000

    Added button, not showing in tab, need fix

---
+++ 2811_cw3/sc17mtab.h
@@ -0,0 +1,136 @@
+#include "globals.h"
+#include "gitpp.h"
+
+#include <string>
+#include <sstream>
+
+#include <iostream>
+#include <string>
+
+#include <QLabel>
+#include <QGridLayout>
+#include <QtWidgets>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QTabWidget>
+#include <QVBoxLayout>
+
+namespace commits
+{
+class listCommits : public QTabWidget{
+  Q_OBJECT
+
+  public:
+    listCommits(){}
+    ~ listCommits(){}
+
+  void Execute()
+  {
+    //Create git path
+    std::string path=".";
+  	GITPP::REPO r(path.c_str());
+
+    //count number of branches
+    int a = 0;
+    for(GITPP::BRANCH i : r.branches())
+    {
+      a = a + 1;
+    }
+
+    //Initialise varaibales
+    QVBoxLayout *mainLayout = new QVBoxLayout;
+    QGridLayout *commitLayout = new QGridLayout;
+    QGridLayout *branchLayout = new QGridLayout;
+
+
+    QLabel *labels[a];
+    QLabel *commitInfo[a];
+    a = 2;
+    if(a>1)
+    {
+      int count = 0;
+
+      //Populate combobox with branches
+      QComboBox *comboBox = new QComboBox;
+      //////////////////////////BUTTON////////////////////////////////////
+      QPushButton *updateButton = new QPushButton("Update Branch", this);
+      updateButton->setText("Button No. 1");
+      QObject::connect(updateButton, SIGNAL(clicked()),this, SLOT(clickedSlot()));
+      updateButton->setSizePolicy(QSizePolicy::Expanding,QSizePolicy::Expanding);
+      mainLayout->addWidget(updateButton);
+      ///////////////////////END OF BUTTON////////////////////////////////
+
+      count = 1;
+
+      for(GITPP::BRANCH i : r.branches())
+      {
+        comboBox->addItem(QString::fromStdString(i.name()));
+      }
+
+      branchLayout->addWidget(comboBox,count+1, 0);
+      branchLayout->addWidget(updateButton, count+1, 1);
+
+      mainLayout->addLayout(branchLayout);
+  }
+
+  int countCommits = 0;
+  for(GITPP::COMMIT i : r.commits())
+  {
+    countCommits = countCommits + 1;
+  }
+
+  //If no commits
+  if(countCommits==0)
+  {
+    labels[0] = new QLabel("No Commits to display!");
+    labels[1] = new QLabel("Add commits to display!");
+
+    commitLayout->addWidget(labels[0], 0, 0);
+    commitLayout->addWidget(labels[1], 1, 0);
+  }
+  else
+  {
+    //List commits (upto to ten)
+    int j = 0;
+    for(auto i : r.commits())
+    {
+      if(j<10)
+      {
+        std::stringstream ss;
+        ss << "<" << i.id() << "> \n <" << i.author() << ">\n <" << i.message() << " \n";
+        std::string s = ss.str();
+
+        //std::string commitDisplay << "<" <<  "> <" << i.author() << "> <" << i.message() << "";
+        labels[j] = new QLabel(tr("%1:").arg(j + 1));
+        commitInfo[j] = new QLabel(QString::fromStdString(s));
+
+        commitLayout->addWidget(labels[j], j, 0);
+        commitLayout->addWidget(commitInfo[j], j, 1);
+        // QLabel* label = new QLabel("str");
+        // std::cout << (j+1)<<". <" << i <<"> <"<< i.signature().name() << "> <" << i.message() << "\n";
+        j = j + 1;
+      }
+      else
+      {
+        break;
+      }
+    }
+  }
+  // mainLayout->addLayout(commitLayout);
+  // setLayout(mainLayout);
+  show();
+  }
+
+public slots:
+  void clickedSlot()
+  {
+    QMessageBox msgBox;
+    msgBox.setWindowTitle("MessageBox Title");
+    msgBox.setText("You Clicked "+ ((QPushButton*)sender())->text());
+    msgBox.exec();
+  }
+};
+INSTALL_TAB(listCommits, __FILE__);
+}
