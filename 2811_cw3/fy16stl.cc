07da72a536c6d11b8b0aa62ecccbb8559bee98f6
Cyrus Leung <fy16stl@leeds.ac.uk>
2018-12-12 22:08:27 +0000

    code/design finished

--- 2811_cw3/fy16stl.cc
+++ 2811_cw3/fy16stl.cc
@@ -1,58 +1,85 @@
 #include "globals.h"
-
-#include <QLabel>
-#include <QtWidgets>
-#include <QComboBox>
 #include "gitpp.h"
-#include <iostream>
+
+#include <string>
 #include <sstream>
+
+#include <iostream>
 #include <string>
 
-namespace{
+#include <QLabel>
+#include <QGridLayout>
+#include <QtWidgets>
+#include <QLineEdit>
+#include <QComboBox>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QVBoxLayout>
+#include <QFileDialog>
+#include <QObject>
+
+namespace repo{
 class createRepositoryTab : public QTabWidget{
-	QVBoxLayout *layout = new QVBoxLayout;
-
-	QLabel *title = new QLabel("Create or select repository");
-	QPushButton *createButton = new QPushButton("Create repository", this);
-
 
 public:	createRepositoryTab(): QTabWidget(){
-	std::string path=".";
-	try{
-		GITPP::REPO r(path.c_str());
-	}
-	catch(GITPP::EXCEPTION_CANT_FIND const&)
-	{
-		QLabel *hi = new QLabel("Hiiii");
-		layout-> addWidget(hi);
-	}
+	// std::string path=".";
+	// GITPP::REPO r(path.c_str());
 
-	title->setAlignment(Qt::AlignCenter);
+	QVBoxLayout *layout = new QVBoxLayout;
+	QGridLayout *grid = new QGridLayout;
 
-	connect(createButton, SIGNAL (released()), this, SLOT (handleCreateButton()));
+	//LABELS//
+	QLabel *title = new QLabel("<h3>Create or select repository<h3>");
+	title-> setAlignment(Qt::AlignCenter);
+
+	QLabel *OR = new QLabel("OR");
+	OR->setAlignment(Qt::AlignCenter);
+
+	//BUTTONS//
+	QPushButton *createButton = new QPushButton("Create repository");
+	QObject::connect(createButton, SIGNAL(released()), SLOT(handleCreateButton));
+	//connect(createButton, SIGNAL(released()), SLOT (handleCreateButton()));
+
+	QPushButton *selectButton = new QPushButton("Select repository", this);
+	connect(createButton, SIGNAL(released()), this, SLOT (handleSelectButton()));
+
+	//COMBOBOX
+	QComboBox *comboBox = new QComboBox;
+	comboBox-> setFont(QFont("Times Roman", 20));
+	comboBox->addItem("REPO1.git");
+	comboBox->addItem("REPO2.git");
+	comboBox->addItem("use file explorer");
+
+	QLineEdit* textField=new QLineEdit();
+	textField->setFont(QFont("Typewriter", 20));
+	textField->setPlaceholderText("Name of new repository");
 
 	layout-> addWidget(title);
-	//layout-> addWidget(hi);
+	layout-> addWidget(textField);
 	layout-> addWidget(createButton);
+	layout-> addWidget(OR);
+	layout-> addWidget(comboBox);
+	layout-> addWidget(selectButton);
+
+	layout->addLayout(grid);
 	setLayout(layout);
 }
+
+public slots:
+	void handleCreateButton(){
+		std::string path=".";
+		GITPP::REPO r(GITPP::REPO::_create, path.c_str());
+		QMessageBox msgBox;
+		msgBox.setText("Success");
+		msgBox.exec();
+	}
+
+	void handleSelectButton(){
+		QFileDialog dialog(this);
+		dialog.setFileMode(QFileDialog::AnyFile);
+	}
+
 };
-
-// class HelloWorldLabel : public QLabel{
-// public:
-// 	HelloWorldLabel() : QLabel(){
-// 		setText("hiiiiiii");
-// 		std::string path=".";
-// 		try{
-// 			GITPP::REPO r(path.c_str());
-// 		}
-// 		catch(GITPP::EXCEPTION_CANT_FIND const&)
-// 		{
-// 			setText("hiii");
-// 		}
-// 	}
-// };
-
-INSTALL_TAB(createRepositoryTab, "Cyrus's Tab");
+INSTALL_TAB(createRepositoryTab, "CreateSelectRepo");
 
 }
